072e68352faed890237265aa22e90eb2e74532b5
Marcos Giovanny <giovannygigi@gmail.com>
2021-11-07 11:40:51 -0300

    Home, Navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,26 @@
+import Navbar from "components/Navbar";
+import Footer from "components/Navbar/footer";
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
-        <div>
-            <h1>Página Home</h1>
+        <>
+        <Navbar />
+
+        
+        <div className="container">
+            <div className="jumbotron">
+                <h1 className="display-4">DSVendas</h1>
+                <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                <hr />
+                <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                <Link className="btn btn-primary btn-lg" to="/dashboard">
+                    Acessar dashboard
+                </Link>
+            </div>
         </div>
+        <Footer />
+        </>
     );
 }
 
